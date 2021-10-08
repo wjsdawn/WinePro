ad2021efe318afffe2582406e4b1a439769846da
wjsdawn <1666938053@qq.com>
2021-10-08 16:40:31 +0800

    painting添加

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var poetRouter = require('./routes/poet')
+var  paintingRouter = require('./routes/painting')
 
 var express = require('express');//这两行通过 require() 导入 Express 模块，并创建了一个 Express 应用
 var app = express();
@@ -30,6 +31,7 @@ app.use('/', indexRouter);
 app.use('/', usersRouter);
 app.use('/',loginRouter);
 app.use('/',poetRouter);
+app.use('/',paintingRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
