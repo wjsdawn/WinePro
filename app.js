a841b51252f261086b9f2977df3de2f6721199e6
wjsdawn <1666938053@qq.com>
2021-10-11 17:52:50 +0800

    更新

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var DrinkingGameRouter = require('./routes/DrinkingGame');
 var WineAndEconomyRouter = require('./routes/WineAndEconomy');
 var WineBrandRouter = require('./routes/WineBrang');
 var WinePicDataRouter = require('./routes/WinePicData');
+var WineContainersInfoRouter = require('./routes/WineContainersInfo');
 
 var express = require('express');//这两行通过 require() 导入 Express 模块，并创建了一个 Express 应用
 var app = express();
@@ -42,6 +43,7 @@ app.use('/',DrinkingGameRouter);
 app.use('/',WineAndEconomyRouter);
 app.use('/',WineBrandRouter);
 app.use('/',WinePicDataRouter);
+app.use('/',WineContainersInfoRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
