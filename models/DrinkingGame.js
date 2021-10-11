2e821ea0b7218e41d085920b73e610f85e496ec7
guohao225 <guohao225@163.com>
2021-10-11 20:40:16 +0800

    提交代码

--- models/DrinkingGame.js
+++ models/DrinkingGame.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 // 新建Schema对象
 const DrinkSchema = new Schema({
     name :String,
-    children : Array,
+    children : Schema.Types.Buffer,
 })
 //使用model 第三个参数为表名 如果第三个参数不设置则默在表名后加s users
 const Models = {
--- routes/DrinkingGame.js
+++ routes/DrinkingGame.js
@@ -2,21 +2,31 @@ var express = require('express');
 var router = express.Router();
 // 导入实列
 let modelpoet = require('../models/DrinkingGame');
-router.post('/DrinkingGame',function(req,res){
-    let params = req.body;
-    let param = {};
-    console.log(param);
-    modelpoet.user.find(param,(err,data) =>{
-        if(err){
-            res.status(500).send()
-            return
-        } else {
-            res.send({
-                state:200,
-                msg:'查询成功',
-                data:data
-            })
-        }
-    });
+modelpoet.user.find({},(err,data) =>{
+    if(err){
+        res.status(500).send()
+        return
+    } else {
+        console.log(data[0]["name"])
+        var jsonData = data[0]["name"].toJson()
+        console.log(jsonData)
+    }
 });
+// router.post('/DrinkingGame',function(req,res){
+//     let params = req.body;
+//     let param = {};
+//     console.log(param);
+//     modelpoet.user.find(param,(err,data) =>{
+//         if(err){
+//             res.status(500).send()
+//             return
+//         } else {
+//             res.send({
+//                 state:200,
+//                 msg:'查询成功',
+//                 data:data
+//             })
+//         }
+//     });
+// });
 module.exports = router;
